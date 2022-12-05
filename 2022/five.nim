214a1e355ada90e7c43941b66bed13da9b3ead56
NiChrosia <nichrosian@gmail.com>
2022-12-05 10:53:39 -0600

    feat: cleanup day 5

--- 2022/five.nim
+++ 2022/five.nim
@@ -1,96 +1,72 @@
-import strutils, algorithm
-
-var boxes = newSeq[string]()
-
-proc pop(s: var string, c: int): string =
-  result = s[s.high - c + 1 .. s.high]
-  s.setLen(s.len - c)
-  
-var test = "abcde"
-discard test.pop(2)
-assert test == "abc"
-
-proc push(s: var string, letters: string) =
-  s &= letters
-    
-proc printBoxes() =
-  var max = 0
-  for box in boxes:
-    if box.len > max:
-      max = box.len
-  
-  for row in countdown(max, 1, 1):
-    for i in boxes.low .. boxes.high:
-      let box = boxes[i]
-      
-      if box.len < row:
-        stdout.write(" ".repeat(3))
-      else:
-        stdout.write("[")
-        stdout.write($box[row - 1])
-        stdout.write("]")
-      
-      if i < boxes.high:
-        stdout.write(" ")
-      else:
-        stdout.write("\n")
-        
-  for i in boxes.low .. boxes.high:
-    stdout.write(" ")
-    stdout.write($(i + 1))
-    stdout.write(" ")
-    
-    if i < boxes.high:
-      stdout.write(" ")
-    else:
-      stdout.write("\n")
-
-proc read(input: string) =
-  let lines = input.split("\n")
-  boxes.setLen((lines[0].len + 1) div 4)
-  
-  for line in lines:
-    if line == "":
-      discard
-    elif line.contains('['):
-      # boxes
-      for i in line.low .. line.high:
-        let c = line[i]
-        
-        if c in 'A' .. 'Z':
-          let box = (i - 1) div 4
-          boxes[box] &= c
-    elif line.contains(" 1   2 "):
-      # finalize boxes
-      for i in boxes.low .. boxes.high:
-        boxes[i] = cast[string](boxes[i].reversed())
-    elif line.contains("move"):
-      # instructions
-      let parts = line.split(" ")
-
-      let count = parts[1].parseInt()
-      let source = parts[3].parseInt()
-      # apparently \r is randomly in the input data
-      let destination = parts[5].replace("\r", "").parseInt()
-      
-      # printBoxes()
-      # echo source, " (", count, ") -> ", destination
-      
-      let letters = boxes[source - 1].pop(count)
-
-      when defined(a):
-        let reversed = cast[string](letters.reversed())
-      else:
-        let reversed = letters
-      
-      boxes[destination - 1].push(reversed)
+import strutils, deques, sequtils
 
 let input = readFile("./data/5.txt")
 
-read(input)
+var boxes: seq[Deque[char]]
+var count = 0
 
-for i in boxes.low .. boxes.high:
-  let box = boxes[i]
-  stdout.write(box[box.high])
+for line in input.split("\n"):
+  if line.contains("["):
+    # box line
+    if count == 0:
+      count = (line.len + 1) div 4
+      
+      for index in 1 .. count:
+        boxes.add(initDeque[char]())
+    
+    for index in boxes.low .. boxes.high:
+      let charIndex = index * 4 + 1
+      let char = line[charIndex]
+      
+      if char == ' ':
+        continue
+      
+      boxes[index].addFirst(char)
+  elif line.contains("move"):
+    # movement command
+    # \r has occasionally been in the input data, so remove it
+    var parts = line.split(" ").mapIt(it.replace("\r", ""))
+    
+    let count = parseInt(parts[1])
+    # indices start with 0
+    let source = parseInt(parts[3]) - 1
+    let dest = parseInt(parts[5]) - 1
+    
+    when defined(a):
+      for i in 1 .. count:
+        boxes[dest].addLast(boxes[source].popLast())
+    else:
+      var queue = initDeque[char]()
+      
+      for i in 1 .. count:
+        queue.addLast(boxes[source].popLast())
+        
+      for i in 1 .. count:
+        boxes[dest].addLast(queue.popLast())
 
-stdout.write("\n")
+var height = 0
+
+for box in boxes:
+  if box.len > height:
+    height = box.len
+
+for row in countdown(height - 1, 0, 1):
+  for index in 0 .. boxes.high:
+    let box = boxes[index]
+    let high = box.len - 1
+    
+    if row > high:
+      # this box doesn't reach this row
+      stdout.write "   "
+    else:
+      # this box does
+      stdout.write "["
+      stdout.write $box[row]
+      stdout.write "]"
+    
+    if index < boxes.high:
+      # write spacing if partially through
+      stdout.write " "
+    else:
+      # otherwise finish it with a newline
+      stdout.write "\n"
