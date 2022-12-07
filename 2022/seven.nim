ee1acbc6d3f3868572632b8589363be582179ea4
NiChrosia <nichrosian@gmail.com>
2022-12-07 10:25:20 -0600

    feat: add functional parser

--- 2022/seven.nim
+++ 2022/seven.nim
@@ -1,51 +1,70 @@
 import tables, strutils
 
-let input = """$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k"""
+let input = readFile("./data/7.txt")
 
 type
     Node = object
         name: string
         size: int
 
-        children: Table[string, Node]
+        parent: ref Node
+        children: OrderedTable[string, Node]
 
 proc isFolder(n: Node): bool =
-    return n.children.len > 0
+    return n.size == 0
 
-var tree = Node(name: "", size: 0)
-var current: Node
+proc `$`(n: Node, indent: int = 0): string =
+    var name: string
+
+    if n.isFolder:
+        name = n.name & "/"
+    else:
+        name = n.name & ": " & $n.size
+
+    result &= " ".repeat(indent) & n.name & "\n"
+    
+    for c in n.children.values:
+        result &= c.`$`(indent + 2)
+
+var current = Node(name: "", children: {"/": Node(name: "/")}.toOrderedTable)
 
 for line in input.split("\n"):
-    if line.contains("$ cd"):
+    if line.contains("$ cd "):
+        # change directory
         let directory = line[5 .. line.high]
 
-        if directory == "/":
-            current = tree
+        if directory == ".." or directory == "..\r":
+            current.parent.children[current.name] = current
+            current = current.parent[]
         else:
-            current = tree[directory]
+            let parent = current
+
+            current = Node(name: directory)
+            current.parent = new(Node)
+            current.parent[] = parent
     elif line.contains("$ ls"):
+        # do nothing
+        discard
+    elif line == "":
+        # ignore empty lines
         discard
     else:
-        discard
+        # add to current node
+        if line.contains("dir "):
+            # directory
+            let name = line[4 .. line.high]
+
+            current.children[name] = Node(name: name)
+        else:
+            # file
+            let space = line.find(" ")
+
+            let size = parseInt(line[0 .. space - 1])
+            let name = line[space + 1 .. line.high]
+
+            current.children[name] = Node(name: name, size: size)
+
+echo "current: "
+echo current
+echo "parent: "
+echo current.parent[]
