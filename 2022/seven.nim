17ebfdd8e0410450eeae3d0ed537eda78c6efaa9
NiChrosia <nichrosian@gmail.com>
2022-12-07 11:30:07 -0600

    feat: finish day 7

--- 2022/seven.nim
+++ 2022/seven.nim
@@ -1,11 +1,11 @@
-import tables, strutils
+import tables, strutils, sequtils, algorithm
 
 let input = readFile("./data/7.txt")
 
 type
     Node = object
         name: string
-        size: int
+        size, summedSize: int
 
         parent: ref Node
         children: OrderedTable[string, Node]
@@ -21,27 +21,61 @@ proc `$`(n: Node, indent: int = 0): string =
     else:
         name = n.name & ": " & $n.size
 
-    result &= " ".repeat(indent) & n.name & "\n"
+    result &= " ".repeat(indent) & name & "\n"
     
     for c in n.children.values:
         result &= c.`$`(indent + 2)
 
 var current = Node(name: "", children: {"/": Node(name: "/")}.toOrderedTable)
 
-for line in input.split("\n"):
+# part a
+var thresholdSum = 0
+
+# part b
+var sums: seq[int]
+
+proc cd(line: string) =
+    # change directory
+    var directory = line[5 .. line.high]
+    directory = directory.replace("\r", "")
+
+    if directory == ".." or directory == "..":
+        current.parent.children[current.name] = current
+        current.parent.summedSize += current.summedSize
+
+        if current.summedSize <= 100_000:
+            thresholdSum += current.summedSize
+
+        sums.add(current.summedSize)
+
+        current = current.parent[]
+    else:
+        let parent = current
+
+        current = Node(name: directory)
+        current.parent = new(Node)
+        current.parent[] = parent
+
+proc addEntry(line: string) =
+    # add to current node
+    if line.contains("dir "):
+        # directory
+        let name = line[4 .. line.high]
+
+        current.children[name] = Node(name: name)
+    else:
+        # file
+        let space = line.find(" ")
+
+        let size = parseInt(line[0 .. space - 1])
+        let name = line[space + 1 .. line.high]
+
+        current.children[name] = Node(name: name, size: size)
+        current.summedSize += size
+
+for line in input.split("\n").mapIt(it.replace("\r", "")):
     if line.contains("$ cd "):
-        # change directory
-        let directory = line[5 .. line.high]
-
-        if directory == ".." or directory == "..\r":
-            current.parent.children[current.name] = current
-            current = current.parent[]
-        else:
-            let parent = current
-
-            current = Node(name: directory)
-            current.parent = new(Node)
-            current.parent[] = parent
+        cd(line)
     elif line.contains("$ ls"):
         # do nothing
         discard
@@ -49,22 +83,23 @@ for line in input.split("\n"):
         # ignore empty lines
         discard
     else:
-        # add to current node
-        if line.contains("dir "):
-            # directory
-            let name = line[4 .. line.high]
+        addEntry(line)
 
-            current.children[name] = Node(name: name)
-        else:
-            # file
-            let space = line.find(" ")
-
-            let size = parseInt(line[0 .. space - 1])
-            let name = line[space + 1 .. line.high]
-
-            current.children[name] = Node(name: name, size: size)
+cd("$ cd ..")
 
 echo "current: "
 echo current
-echo "parent: "
-echo current.parent[]
+
+# part a
+echo "threshold sum: ", thresholdSum
+
+# part b
+sums.sort()
+
+let unused = 70_000_000 - current.summedSize
+let required = 30_000_000 - unused
+
+for sum in sums:
+    if sum >= required:
+        echo "smallest directory to delete: ", sum
+        break
